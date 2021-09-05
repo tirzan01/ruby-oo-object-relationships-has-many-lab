69cdaabf6795e1d8fb96b0cd935a098e9a2a0233
thomas <thomasmonta98@gmail.com>
2021-09-05 22:15:44 +1000

    done with assignment

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+
+  attr_accessor :author
+  attr_reader :title
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+
+  attr_accessor :artist
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+end
